33277973e2664e1f31bc447aea5378555bfc0c6a
chlee <chlee@cretem.co.kr>
2025-03-09 22:41:47 +0900

    first

---
+++ XC8Application1/XC8Application1/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 3/8/2025 8:02:15 AM
+ *  Author: user
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
